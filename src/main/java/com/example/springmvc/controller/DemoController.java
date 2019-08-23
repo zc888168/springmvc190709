a46c00f46e2d693553c409a79f6f549c70c33650
chaozhang17 <chaozhang17@iflytek.com>
2019-08-23 23:03:14 +0800

    add shutdown Endpoints

--- src/main/java/com/example/springmvc/controller/DemoController.java
+++ src/main/java/com/example/springmvc/controller/DemoController.java
@@ -1,8 +1,13 @@
 package com.example.springmvc.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/demo")
@@ -12,4 +17,17 @@ public class DemoController {
     public String index(){
         return "hello";
     }
+    @PostMapping("/login")
+    public String login(HttpServletRequest request){
+        String name = request.getParameter("name");
+        System.out.println("name " + name);
+        request.getSession().setAttribute("name" , name);
+        return name;
+    }
+    @PostMapping("/check")
+    public String check(HttpServletRequest request){
+       String name = (String) request.getSession().getAttribute("name");
+        return name;
+    }
+
 }
